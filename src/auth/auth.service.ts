b0861b3877a551f10876c9d5d3a5bdf5b4134c28
Levente Danel <danellevente@yahoo.com>
2020-08-18 20:07:09 +0300

    implemented  the signup feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.userRepository.signUp(authCredentialsDto);
+  }
+}
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -6,6 +6,7 @@ import {
   Unique,
   OneToMany,
 } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 import { Task } from 'src/tasks/task.entity';
 
 @Entity()
@@ -29,4 +30,9 @@ export class User extends BaseEntity {
     { eager: true },
   )
   tasks: Task[];
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }

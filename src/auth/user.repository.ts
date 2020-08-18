fa8d61669ede242d0f30f9834f577cd1ed530e68
Levente Danel <danellevente@yahoo.com>
2020-08-18 20:00:16 +0300

    created user repository

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,47 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+import * as bcrypt from 'bcryptjs';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+
+    const user = new User();
+    user.username = username;
+    user.password = await bcrypt.hash(password, salt);
+    user.salt = salt;
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+}

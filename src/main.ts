dfb40b6ed2238b90a5d334aed9fdd42777cc142a
Levente Danel <danellevente@yahoo.com>
2020-10-16 12:36:27 +0300

    add security configs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import * as helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(helmet());
+  app.set('trust proxy', 1);
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
